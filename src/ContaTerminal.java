e313eb5127535792a1211f0949f35b0088994422
Jose Saraceni Vitor <jose.saraceni@chg>
2024-04-25 17:26:40 -0300

    second commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
-	public static void main(String[] args) {
-		System.out.println("Teste");
-	}
 
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);  // Create a Scanner object
+
+        System.out.println("Olá, vamos iniciar o cadastro da sua conta.");
+        
+        System.out.println("Digite o número da sua Agencia: ");
+        String Agencia = scan.nextLine();
+        
+        System.out.println("Conta: ");
+        int Conta = Integer.parseInt(scan.nextLine());
+        
+        System.out.println("Nome Completo: ");
+        String Nome = scan.nextLine();
+        
+        System.out.println("Saldo: ");
+        Float Saldo = Float.parseFloat(scan.nextLine());
+        
+        System.out.println("Olá " + Nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + Agencia + ", Conta " + Conta + " e seu saldo de R$" + Saldo + " já está disponível para saque");
+    }
 }
